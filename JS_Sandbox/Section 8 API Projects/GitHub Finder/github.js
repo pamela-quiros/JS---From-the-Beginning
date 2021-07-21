af920e4f352267f952dcc5b33dcae3670209ce8a
pamela_quiros <pamela.quiros.urena@cr.ey.com>
2021-07-20 19:43:32 -0600

    API Projects - GitHub Finder

---
+++ JS_Sandbox/Section 8 API Projects/GitHub Finder/github.js
@@ -0,0 +1,21 @@
+class Github {
+    constructor() {
+        this.client_id = 'ffb2138dc3ef6f5b52fa';
+        this.client_secret = 'cfdae071eddda332d125bfe610093095055a180c';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await reposResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
